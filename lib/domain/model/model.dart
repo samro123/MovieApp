b8507d93e0bce8b9d5920b17925368d9b7f6118b
samro123 <thsam.20it5@vku.udn.vn>
2024-10-19 20:34:00 +0700

    Login function finish

--- lib/domain/model/model.dart
+++ lib/domain/model/model.dart
@@ -4,3 +4,8 @@ class SliderObject {
   String image;
   SliderObject(this.title, this.subTitle, this.image);
 }
+class Authentication{
+  String token;
+  bool authenticated;
+  Authentication(this.token, this.authenticated);
+}
